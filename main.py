a393dc6cf7acb67c639b1abca500da52fed84aac
Pierre Vanvolsem <pierre@vanvolsem.com>
2020-09-19 17:47:14 +0200

    Next Week, Weekend functionality
    Clean up
    Add param to see next week
    Add If weekend then show next week schedule
    Type,Faculteit,Opleiding are var now

--- main.py
+++ main.py
@@ -1,42 +1,48 @@
 ﻿from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import pendulum
 from selenium.webdriver.support.ui import WebDriverWait
 from time import sleep
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+import datetime
+import sys
 
-months = ['jan', 'feb', 'mrt', 'apr', 'mei', 'jun', 'aug', 'sep', 'okt', 'nov', 'dec']
-
-
-def translate_numeric_month_to_dutch_full(number):
-    return months[number - 1]
-
-
-def navigate_to_schedule():
-    driver = webdriver.Edge('msedgedriver.exe')
+def navigate_to_schedule_overview():
     driver.get('https://splus.cumulus.vub.ac.be/SWS/v3/evenjr/NL/STUDENTSET/studentset.aspx')
 
-    driver.find_element_by_id('SCH').click()
-    driver.find_element_by_id('WE').click()
-    driver.find_element_by_id('SWS_SCH_WE_NL_RS_Computerwet_SET').click()
+    Type="Schakel- en Voorbereidingsprogramma's"
+    Faculteit="Wetenschappen en Bio-ingenieurswetenschappen"
+    Opleiding="Computerwetenschappen"
+    driver.find_element(By.XPATH, '//td[text()="'+Type+'"]').click()
+    driver.find_element(By.XPATH, '//td[text()="'+Faculteit+'"]').click()
+    driver.find_element(By.XPATH, '//td[text()="'+Opleiding+'"]').click()
 
-    today = pendulum.now()
-    first_date_of_week = today.start_of('week')
-    day = str(first_date_of_week.day)
-    month = translate_numeric_month_to_dutch_full(first_date_of_week.month)
-    year = str(first_date_of_week.year)
-
+def navigate_to_schedule():    
     list_weeks = driver.find_element_by_id('lbWeeks')
     list_weeks_select = Select(list_weeks)
     list_weeks_select.deselect_all()
 
     query_string = 'Deze week'
     elems = list_weeks.find_elements_by_xpath("//*[contains(text(),'" + query_string + "')]")
-    elems[0].click()
+
+    this_week_value = int(elems[0].get_property("value"))
+    this_week_value += calculate_amount_weeks_to_add()
+    list_weeks.find_element_by_xpath("//option[@value=' "+str(this_week_value)+"']").click()
     driver.find_element_by_id('bGetTimetable').click()
 
+def calculate_amount_weeks_to_add():
+    # todo try catch
+    weeks_to_add=0
+    # if exaactly one argument is given
+    if len(sys.argv)==2:
+        weeks_to_add=int(sys.argv[0])
+
+    #add 1 if weekend
+    if datetime.datetime.today().weekday() >=5:
+        weeks_to_add+=1
+    return weeks_to_add
 
 if __name__ == '__main__':
+    driver = webdriver.Edge('msedgedriver.exe')
+    navigate_to_schedule_overview()
     navigate_to_schedule()
-    while True:
-        pass
